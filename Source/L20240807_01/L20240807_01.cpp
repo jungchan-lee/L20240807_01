78c6e3956f563dbcbe299cf96c3137f7777050f7
jungchan-lee <xlloew@gmail.com>
2024-08-07 14:43:35 +0900

    add c++ class

---
+++ Source/L20240807_01/L20240807_01.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "L20240807_01.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, L20240807_01, "L20240807_01" );
